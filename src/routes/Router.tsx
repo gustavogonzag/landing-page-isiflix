54a5a6d1957e164cc4f1cbc89824d25b81d0a87e
gustavogonzag <gustavosg91@gmail.com>
2025-10-03 21:23:21 -0300

    fix: routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,19 +1,17 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
 import PageHome from "../Pages/PageHome/PageHome";
 import AboutUs from "../Pages/AboutUs/AboutUs";
 import ForBusiness from "../Pages/ForBusiness/ForBusiness";
 import MyCareer from "../Pages/MyCareer/MyCareer";
+import { Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => {
   return (
-    <HashRouter>
       <Routes>
         <Route path="/" element={<PageHome />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/mycareer" element={<MyCareer />} />
         <Route path="/forbusiness" element={<ForBusiness />} />
       </Routes>
-    </HashRouter>
   );
 };
 
