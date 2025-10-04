df27b8e1fac209c6f4cb52462ea76c3f8f3f709e
gustavogonzag <gustavosg91@gmail.com>
2025-10-03 21:11:53 -0300

    feat: hasg router

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import PageHome from "../Pages/PageHome/PageHome";
 import AboutUs from "../Pages/AboutUs/AboutUs";
 import ForBusiness from "../Pages/ForBusiness/ForBusiness";
@@ -6,12 +6,14 @@ import MyCareer from "../Pages/MyCareer/MyCareer";
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<PageHome />} />
-      <Route path="/aboutus" element={<AboutUs />} />
-      <Route path="/mycareer" element={<MyCareer />} />
-      <Route path="/forbusiness" element={<ForBusiness />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<PageHome />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/mycareer" element={<MyCareer />} />
+        <Route path="/forbusiness" element={<ForBusiness />} />
+      </Routes>
+    </HashRouter>
   );
 };
 
